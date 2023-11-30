da57a460ec2278df825ced53cca30050f14d3b2b
Joseph Cutler <jwc@seas.upenn.edu>
2023-11-29 19:19:05 -0500

    fix bug in lower

--- src/Frontend/ElabSyntax.hs
+++ src/Frontend/ElabSyntax.hs
@@ -1,11 +1,11 @@
-{-# LANGUAGE FlexibleContexts, FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts, FlexibleInstances, BangPatterns #-}
 {-# LANGUAGE LambdaCase #-}
 module Frontend.ElabSyntax (doElab, Term(..), Program, Cmd(..), elabTests) where
 
 import Values ( Lit(..) )
 import Var (Var(..), TyVar, FunVar)
-import Control.Monad.State (MonadState (put), get, StateT (runStateT))
-import Control.Monad.Except (MonadError (throwError), ExceptT, runExceptT)
+import Control.Monad.State (MonadState (put), get, StateT (runStateT), evalStateT)
+import Control.Monad.Except (MonadError (throwError), ExceptT, runExceptT, Except)
 import Types
 
 import qualified Frontend.SurfaceSyntax as Surf
@@ -14,13 +14,30 @@ import Util.PrettyPrint (PrettyPrint(..))
 import qualified Data.Map as M
 import qualified Data.Set as S
 import Control.Monad.RWS.Strict (MonadReader (local, ask))
-import Control.Monad.Reader (ReaderT (runReaderT), asks)
+import Control.Monad.Reader (ReaderT (runReaderT), asks, runReader)
 import Test.HUnit
 import Data.List (intercalate)
 import qualified Data.Functor
 import Data.Functor ((<&>))
 import qualified HistPgm as Hist
-import qualified Data.List as Hist
+import Debug.Trace (trace)
+
+{-
+
+This module is a general elaboration pass on the surface syntax, taking care of any purely syntactic transformation we need handled.
+Note that this does not emit core terms: going to core terms requires type information. This pass handles all the translations that
+don't require any type information.
+
+The pass includes:
+
+
+1. Resolving function calls as either (a) recursive calls, or (b) calls to functions defined earlier in the file.
+2. Precomposing elimination forms with cuts to go from the natural deduction surface syntax to the sequent calculus of the elaborated syntax.
+For example, we translate (let (a;b) = e1 in e2) to (let z = e1 in (let (a;b) = z in e2)), for fresh z.
+3. Renaming variables to eliminate shadowing. This just requires generating fresh a fresh name when we see that a shadow is happening,
+and then replacing the shadowed name with the fresh name in the body.
+
+-}
 
 data Term =
       TmLitR Lit
@@ -69,141 +86,6 @@ instance PrettyPrint Term where
             go False (TmFunCall f ts es) = concat [pp f,"[",intercalate "," (map pp ts),"]","(", pp (go False <$> es), ")"]
             go False (TmWait x e) = concat ["wait ", pp x," do ", go True e]
 
-data ElabState = ES { nextVar :: Int } deriving (Eq, Ord, Show)
-
-data ElabErr =
-      UnboundVar Var
-    | EqualBoundVars Var
-    | PolymorphicRec Var.FunVar
-    deriving (Eq, Ord, Show)
-
-instance PrettyPrint ElabErr where
-    pp (UnboundVar (Var x)) = concat ["Variable ",x," not bound. This is a compiler bug."]
-    pp (EqualBoundVars x) = concat ["Binding two copies of the same variable ",pp x]
-    pp (PolymorphicRec f) = concat ["Polmorphic recursive calls not supported (function ",pp f,")"]
-
-data ElabInput = EI {renaming :: M.Map Var Var, funName :: Var.FunVar}
-
-class (MonadState ElabState m, MonadReader ElabInput m, MonadError ElabErr m) => ElabM m where
-
-
-freshElabVar :: (ElabM m) => m Var
-freshElabVar = do
-    es <- get
-    put $ ES (nextVar es + 1)
-    return $ Var.Var $ "__x" ++ show (nextVar es)
-
-withUnshadow :: (ElabM m) => Maybe Var -> m a -> m (a,Var)
-withUnshadow Nothing u = do
-    x <- freshElabVar
-    a <- local (\(EI sm l) -> EI (M.insert x x sm) l) u
-    return (a,x)
-withUnshadow (Just x) u = do
-    EI sm _ <- ask
-    if M.member x sm then do
-        y <- freshElabVar
-        a <- local (\(EI sm' fn) -> EI (M.insert x y sm') fn) u
-        return (a,y)
-    else do
-        a <- local (\(EI sm' fn) -> EI (M.insert x x sm') fn) u
-        return (a,x)
-
-unshadow :: (ElabM m) => Var -> m Var
-unshadow x = do
-    EI sm _ <- ask
-    case M.lookup x sm of
-        Just y -> return y
-        Nothing -> throwError (UnboundVar x)
-
-sameBinder :: Maybe Var -> Maybe Var -> Maybe Var
-sameBinder Nothing _ = Nothing
-sameBinder _ Nothing = Nothing
-sameBinder (Just x) (Just y) = if x == y then Just x else Nothing
-
-elab :: (ElabM m) => Surf.Term -> m Term
-elab (Surf.TmLitR l) = return (TmLitR l)
-elab Surf.TmEpsR = return TmEpsR
-elab (Surf.TmHistPgm he) = TmHistPgm <$> elabHist he
-elab (Surf.TmVar x) = TmVar <$> unshadow x
-elab (Surf.TmCatL mx my e1 e2) = do
-    case sameBinder mx my of
-        Just x -> throwError (EqualBoundVars x)
-        _ -> return ()
-    e1' <- elab e1
-    ((e2',y),x) <- withUnshadow mx $ withUnshadow my $ elab e2
-    z <- freshElabVar
-    return $ TmCut z e1' (TmCatL x y z e2')
-elab (Surf.TmCatR e1 e2) = do
-    e1' <- elab e1
-    e2' <- elab e2
-    return (TmCatR e1' e2')
-elab (Surf.TmParL mx my e1 e2) = do
-    case sameBinder mx my of
-        Just x -> throwError (EqualBoundVars x)
-        _ -> return ()
-    e1' <- elab e1
-    ((e2',y),x) <- withUnshadow mx $ withUnshadow my $ elab e2
-    z <- freshElabVar
-    return $ TmCut z e1' (TmParL x y z e2')
-elab (Surf.TmParR e1 e2) = do
-    e1' <- elab e1
-    e2' <- elab e2
-    return (TmParR e1' e2')
-elab (Surf.TmInl e) = TmInl <$> elab e
-elab (Surf.TmInr e) = TmInr <$> elab e
-elab (Surf.TmIte e e1 e2) = do
-    e' <- elab e
-    e1' <- elab e1
-    e2' <- elab e2
-    z <- freshElabVar
-    return $ TmCut z e' (TmIte z e1' e2')
-elab (Surf.TmPlusCase e mx e1 my e2) = do
-    e' <- elab e
-    (e1',x) <- withUnshadow mx $ elab e1
-    (e2',y) <- withUnshadow my $ elab e2
-    z <- freshElabVar
-    return $ TmCut z e' (TmPlusCase z x e1' y e2')
-elab Surf.TmNil = return TmNil
-elab (Surf.TmCons e1 e2) = TmCons <$> elab e1 <*> elab e2
-elab (Surf.TmStarCase e e1 mx mxs e2) = do
-    e' <- elab e
-    e1' <- elab e1
-    ((e2',xs),x) <- withUnshadow mx $ withUnshadow mxs $ elab e2
-    z <- freshElabVar
-    return $ TmCut z e' (TmStarCase z e1' x xs e2')
-elab (Surf.TmFunCall f ts es) = do
-    curF <- asks funName
-    if f == curF then 
-        if not (null ts) then throwError (PolymorphicRec f) else TmRec <$> mapM elab es
-    else TmFunCall f ts <$> mapM elab es
-elab (Surf.TmWait xs e) = do
-    ys <- mapM unshadow xs
-    go ys <$> elab e
-    where
-        go [] e' = e'
-        go (x:xs') e' = TmWait x (go xs' e')
-elab (Surf.TmCut x e1 e2) = do
-    e1' <- elab e1
-    (e2',x') <- withUnshadow (Just x) $ elab e2
-    return (TmCut x' e1' e2')
-
-elabHist :: (ElabM m) => Hist.Term -> m Hist.Term
-elabHist (Hist.TmVar x) = Hist.TmVar <$> unshadow x
-elabHist e@(Hist.TmValue _) = return e
-elabHist e@Hist.TmEps = return e
-elabHist (Hist.TmBinOp b e1 e2) = Hist.TmBinOp b <$> elabHist e1 <*> elabHist e2
-elabHist (Hist.TmMonOp m e) = Hist.TmMonOp m <$> elabHist e
-elabHist (Hist.TmPair e1 e2) = Hist.TmPair <$> elabHist e1 <*> elabHist e2
-elabHist (Hist.TmInl e) = Hist.TmInl <$> elabHist e
-elabHist (Hist.TmInr e) = Hist.TmInr <$> elabHist e
-elabHist e@Hist.TmNil = return e
-elabHist (Hist.TmCons e1 e2) = Hist.TmCons <$> elabHist e1 <*> elabHist e2
-elabHist (Hist.TmIte e e1 e2) = Hist.TmIte <$> elabHist e <*> elabHist e1 <*> elabHist e2
-
-{-TODO: ensure that fundefs have unique vars-}
-
-instance ElabM (StateT ElabState (ReaderT ElabInput (ExceptT ElabErr Identity))) where
-
 data Cmd =
       FunDef Var.FunVar [Var.TyVar] (Ctx Var.Var (TyF Var.TyVar)) (TyF Var.TyVar) Term
     | SpecializeCommand Var.FunVar [Ty]
@@ -213,22 +95,241 @@ data Cmd =
 
 type Program = [Cmd]
 
-initShadowMap g =
-    let bindings = ctxBindings g in
-    let ks = M.keysSet bindings in
-    S.fold (\x -> M.insert x x) M.empty ks
+
+data ElabErr =
+      UnboundVar Var
+    | EqualBoundVars Var
+    | PolymorphicRec Var.FunVar
+    deriving (Eq, Ord, Show)
+
+instance PrettyPrint ElabErr where
+    pp (UnboundVar x) = concat ["Variable ",pp x," not bound. This is a compiler bug."]
+    pp (EqualBoundVars x) = concat ["Binding two copies of the same variable ",pp x]
+    pp (PolymorphicRec f) = concat ["Polmorphic recursive calls not supported (function ",pp f,")"]
+
+data UnshadowInput = EI {renaming :: M.Map Var Var, funName :: Var.FunVar }
+
+class (MonadState Int m, MonadReader UnshadowInput m, MonadError ElabErr m) => UnshadowM m where
 
 
-elabSingle :: Var.FunVar -> Surf.Term -> S.Set Var -> Either ElabErr (Term, ElabState)
-elabSingle f e s = runIdentity (runExceptT (runReaderT (runStateT (elab e) (ES 0)) $ initInput))
+freshVar :: (MonadState Int m) => String -> m Var
+freshVar x = do
+    n <- get
+    put (n + 1)
+    return (Var.Var ("_" ++ x ++ show n))
+
+{-
+`lowerSurf` mplements phases 1 and 2 of elaboration: lowers the surface language to elaboration language by
+resolving recursive calls, inserting cuts, and turning waits of sequences of variables into a sequence of waits.
+-}
+
+lowerPattern :: (MonadState Int m) => Maybe Var -> m Var
+lowerPattern (Just x) = return x
+lowerPattern Nothing = do
+    n <- get
+    put (n + 1)
+    return $ Var.Var $ "_w" ++ show n
+
+freshCutVar :: (MonadState Int m) => m Var
+freshCutVar = freshVar "c"
+
+
+sameBinder :: Maybe Var -> Maybe Var -> Maybe Var
+sameBinder Nothing _ = Nothing
+sameBinder _ Nothing = Nothing
+sameBinder (Just x) (Just y) = if x == y then Just x else Nothing
+
+lowerSurf :: (MonadState Int m, MonadReader Var.FunVar m, MonadError ElabErr m) => Surf.Term -> m Term
+lowerSurf (Surf.TmLitR l) = return (TmLitR l)
+lowerSurf Surf.TmEpsR = return TmEpsR
+lowerSurf (Surf.TmVar x) = return (TmVar x)
+lowerSurf (Surf.TmCatL mx my e1 e2) = do
+    case sameBinder mx my of
+        Just x -> throwError (EqualBoundVars x)
+        _ -> return ()
+    x <- lowerPattern mx
+    y <- lowerPattern my
+    e1' <- lowerSurf e1
+    e2' <- lowerSurf e2
+    z <- freshCutVar
+    return $ TmCut z e1' (TmCatL x y z e2')
+lowerSurf (Surf.TmCatR e1 e2) = do
+    e1' <- lowerSurf e1
+    e2' <- lowerSurf e2
+    return (TmCatR e1' e2')
+lowerSurf (Surf.TmParL mx my e1 e2) = do
+    case sameBinder mx my of
+        Just x -> throwError (EqualBoundVars x)
+        _ -> return ()
+    x <- lowerPattern mx
+    y <- lowerPattern my
+    e1' <- lowerSurf e1
+    e2' <- lowerSurf e2
+    z <- freshCutVar
+    return $ TmCut z e1' (TmParL x y z e2')
+lowerSurf (Surf.TmParR e1 e2) = do
+    e1' <- lowerSurf e1
+    e2' <- lowerSurf e2
+    return (TmParR e1' e2')
+lowerSurf (Surf.TmInl e) = TmInl <$> lowerSurf e
+lowerSurf (Surf.TmInr e) = TmInr <$> lowerSurf e
+lowerSurf (Surf.TmIte e e1 e2) = do
+    e' <- lowerSurf e
+    e1' <- lowerSurf e1
+    e2' <- lowerSurf e2
+    z <- freshCutVar
+    return $ TmCut z e' (TmIte z e1' e2')
+lowerSurf (Surf.TmPlusCase e mx e1 my e2) = do
+    e' <- lowerSurf e
+    x <- lowerPattern mx
+    y <- lowerPattern my
+    e1'<- lowerSurf e1
+    e2'<- lowerSurf e2
+    z <- freshCutVar
+    return $ TmCut z e' (TmPlusCase z x e1' y e2')
+lowerSurf Surf.TmNil = return TmNil
+lowerSurf (Surf.TmCons e1 e2) = TmCons <$> lowerSurf e1 <*> lowerSurf e2
+lowerSurf (Surf.TmStarCase e e1 mx mxs e2) = do
+    e' <- lowerSurf e
+    e1' <- lowerSurf e1
+    x <- lowerPattern mx
+    xs <- lowerPattern mxs
+    e2' <- lowerSurf e2
+    z <- freshCutVar
+    return $ TmCut z e' (TmStarCase z e1' x xs e2')
+lowerSurf (Surf.TmWait xs e) = do
+    go xs <$> lowerSurf e
     where
-        initInput = EI (S.fold (\x -> M.insert x x) M.empty s) f
+        go [] e' = e'
+        go (x:xs') e' = TmWait x (go xs' e')
+lowerSurf (Surf.TmHistPgm he) = return (TmHistPgm he)
+lowerSurf (Surf.TmCut x e1 e2) = do
+    e1' <- lowerSurf e1
+    e2' <- lowerSurf e2
+    return (TmCut x e1' e2')
+lowerSurf (Surf.TmFunCall f ts es) = do
+    curF <- ask
+    if f == curF then 
+        if not (null ts) then throwError (PolymorphicRec f) else TmRec <$> mapM lowerSurf es
+    else TmFunCall f ts <$> mapM lowerSurf es
+
+
+freshUnshadowVar :: (UnshadowM m) => m Var
+freshUnshadowVar = freshVar "u"
+
+
+withUnshadow :: (UnshadowM m) => Var -> m a -> m (a,Var)
+withUnshadow x u = do
+    EI sm _ <- ask
+    y <- if M.member x sm then freshUnshadowVar else return x
+    a <- local (\(EI sm' fn) -> EI (M.insert x y sm') fn) u
+    return (a,y)
+    -- if M.member x sm then do
+    --     y <- freshUnshadowVar
+    --     a <- local (\(EI sm' fn) -> EI (M.insert x y sm') fn) u
+    --     return (a,y)
+    -- else do
+    --     a <- local (\(EI sm' fn) -> EI (M.insert x x sm') fn) u
+    --     return (a,x)
+
+unshadowVar :: (UnshadowM m) => Var -> m Var
+unshadowVar x = do
+    EI sm _ <- ask
+    case M.lookup x sm of
+        Just y -> return y
+        Nothing -> throwError (UnboundVar x)
+
+{-
+Carry around a map of variables to variables, mapping the source string to the (known-fresh) variable that it should be replaced with.
+This starts with mapping every free variable to itself. Once we notice a variable is shadowed, the map gets updated, mapping the bound
+variable to a fresh name.
+
+Every time we encounter a binder, we run `withUnshadow` before checking the continuation.
+This rebinds all occurrences of the binder with a fresh variable if the binder was not fresh.
+Every use of a variable is then transformed by a call to unshadowVar, which looks up the fresh variable it's mapped ot.
+-}
+unshadowTerm :: (UnshadowM m) => Term -> m Term
+unshadowTerm (TmLitR l) = return (TmLitR l)
+unshadowTerm TmEpsR = return TmEpsR
+unshadowTerm (TmHistPgm he) = TmHistPgm <$> unshadowHist he
+unshadowTerm (TmVar x) = TmVar <$> unshadowVar x
+unshadowTerm (TmCatL x y z e) = do
+    z' <- unshadowVar z
+    ((e',y'),x') <- withUnshadow x $ withUnshadow y $ unshadowTerm e
+    return (TmCatL x' y' z' e')
+unshadowTerm (TmCatR e1 e2) = do
+    e1' <- unshadowTerm e1
+    e2' <- unshadowTerm e2
+    return (TmCatR e1' e2')
+unshadowTerm (TmParL x y z e) = do
+    z' <- unshadowVar z
+    ((e',y'),x') <- withUnshadow x $ withUnshadow y $ unshadowTerm e
+    return (TmParL x' y' z' e')
+unshadowTerm (TmParR e1 e2) = do
+    e1' <- unshadowTerm e1
+    e2' <- unshadowTerm e2
+    return (TmParR e1' e2')
+unshadowTerm (TmInl e) = TmInl <$> unshadowTerm e
+unshadowTerm (TmInr e) = TmInr <$> unshadowTerm e
+unshadowTerm (TmIte z e1 e2) = do
+    z' <- unshadowVar z
+    e1' <- unshadowTerm e1
+    e2' <- unshadowTerm e2
+    return (TmIte z' e1' e2')
+unshadowTerm (TmPlusCase z x e1 y e2) = do
+    z' <- unshadowVar z
+    (e1',x') <- withUnshadow x $ unshadowTerm e1
+    (e2',y') <- withUnshadow y $ unshadowTerm e2
+    return (TmPlusCase z' x' e1' y' e2')
+unshadowTerm TmNil = return TmNil
+unshadowTerm (TmCons e1 e2) = TmCons <$> unshadowTerm e1 <*> unshadowTerm e2
+unshadowTerm (TmStarCase z e1 x xs e2) = do
+    z' <- unshadowVar z
+    e1' <- unshadowTerm e1
+    ((e2',xs'),x') <- withUnshadow x $ withUnshadow xs $ unshadowTerm e2
+    return (TmStarCase z' e1' x' xs' e2')
+unshadowTerm (TmFunCall f ts es) = do
+    es' <- mapM unshadowTerm es
+    return (TmFunCall f ts es')
+unshadowTerm (TmWait x e) = TmWait <$> unshadowVar x <*> unshadowTerm e
+unshadowTerm (TmCut x e1 e2) = do
+    e1' <- unshadowTerm e1
+    (e2',x') <- withUnshadow x (unshadowTerm e2)
+    return (TmCut x' e1' e2')
+unshadowTerm (TmRec e) = TmRec <$> mapM unshadowTerm e
+
+unshadowHist :: (UnshadowM m) => Hist.Term -> m Hist.Term
+unshadowHist (Hist.TmVar x) = Hist.TmVar <$> unshadowVar x
+unshadowHist e@(Hist.TmValue _) = return e
+unshadowHist e@Hist.TmEps = return e
+unshadowHist (Hist.TmBinOp b e1 e2) = Hist.TmBinOp b <$> unshadowHist e1 <*> unshadowHist e2
+unshadowHist (Hist.TmMonOp m e) = Hist.TmMonOp m <$> unshadowHist e
+unshadowHist (Hist.TmPair e1 e2) = Hist.TmPair <$> unshadowHist e1 <*> unshadowHist e2
+unshadowHist (Hist.TmInl e) = Hist.TmInl <$> unshadowHist e
+unshadowHist (Hist.TmInr e) = Hist.TmInr <$> unshadowHist e
+unshadowHist e@Hist.TmNil = return e
+unshadowHist (Hist.TmCons e1 e2) = Hist.TmCons <$> unshadowHist e1 <*> unshadowHist e2
+unshadowHist (Hist.TmIte e e1 e2) = Hist.TmIte <$> unshadowHist e <*> unshadowHist e1 <*> unshadowHist e2
+
+-- freshUnshadowVar :: (UnshadowM m) => m Var
+-- freshUnshadowVar = freshVar "u"
+
+instance UnshadowM (StateT Int (ReaderT UnshadowInput (Either ElabErr))) where
+
+
+elabSingle :: Var.FunVar -> Surf.Term -> S.Set Var -> Either ElabErr Term
+elabSingle f e s = do
+    lowered_e <- runReaderT (evalStateT (lowerSurf e) 0) f
+    unshadowed_e <- runReaderT (evalStateT (unshadowTerm lowered_e) 0) initUnshadowMap
+    return unshadowed_e
+    where
+        initUnshadowMap = EI (S.fold (\x -> M.insert x x) M.empty s) f
 
 doElab :: Surf.Program -> IO Program
 doElab = mapM $ \case
                     (Surf.FunDef f tvs g s e) ->
                         case elabSingle f e (M.keysSet $ ctxBindings g) of
-                            Right (e',_) -> do
+                            Right e' -> do
                                 putStrLn $ "Function " ++ pp f ++ " elaborated OK. Elab term: " ++ pp e' ++ "\n"
                                 return (FunDef f tvs g s e')
                             Left err -> error (pp err)
@@ -252,7 +353,7 @@ elabTests = TestList [
     where
         elabTest e e'' xs = TestCase $ do
             case elabSingle undefined e (S.fromList $ Var.Var <$> xs) of
-                Right (e',_) -> assertEqual "" e' e''
+                Right e' -> assertEqual "" e' e''
                 Left err -> assertFailure (pp err)
         elabFails e xs = TestCase $ do
             case elabSingle undefined e (S.fromList $ Var.Var <$> xs) of
